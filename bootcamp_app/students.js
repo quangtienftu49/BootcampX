12cb6d1f5329434d077940a9ee7b7172cc13ff65
quangtienftu49 <quangtienftu49@gmail.com>
2022-01-06 03:27:11 +0000

    Final submission

---
+++ bootcamp_app/students.js
@@ -0,0 +1,35 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+//   })
+// });
+
+const cohort = process.argv[2];
+const limit = process.argv[3];
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = $1
+LIMIT $2;
+`, [cohort, limit])
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+});
